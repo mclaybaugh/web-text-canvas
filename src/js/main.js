ae7d3187d2f3c9cd291788c1a5c7c235fcf0ce85
Michael Claybaugh <mclaybaugh2@liberty.edu>
2018-08-28 09:00:15 -0400

    removed semicolons

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,50 @@
 import { GameField } from './map'
 
 // 26 rows, 99 columns
-var main_screen = new GameField('A', '-', 26, 99);
+var main_screen = new GameField('A', '-', 26, 99)
 
 function draw_window () {
-    document.getElementById('game-window').innerText = main_screen.content_string;
+    document.getElementById('game-window').innerText = main_screen.content_string
 }
 
 document.addEventListener('keydown', function (event) {
     if (event.defaultPrevented) {
-        return; // Do nothing if the event was already processed
+        return // Do nothing if the event was already processed
     }
     switch (event.which) {
     case 40: // down arrow
     case 74: // j
-        move_down();
-        break;
+        move_down()
+        break
     case 38: // up arrow
     case 75: // k
-        move_up();
-        break;
+        move_up()
+        break
     case 37: // left arrow
     case 72: // h
-        move_left();
-        break;
+        move_left()
+        break
     case 39: // right arrow
     case 76: // l
-        move_right();
-        break;
+        move_right()
+        break
     default:
-        return; // Quit when this doesn't handle the key event.
+        return // Quit when this doesn't handle the key event.
     }
 
     // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-}, true);
+    event.preventDefault()
+}, true)
 
 function move_right () {
-    draw_window();
+    draw_window()
 }
 function move_left () {
-    draw_window();
+    draw_window()
 }
 function move_up () {
-    draw_window();
+    draw_window()
 }
 function move_down () {
-    draw_window();
+    draw_window()
 }
