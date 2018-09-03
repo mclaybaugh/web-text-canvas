b024e95adfc8643fde6854395262187ecabe1b89
Michael Claybaugh <michael.claybaugh@gmail.com>
2018-09-03 18:39:29 -0400

    Made Coord and MapObject classes for drawing

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const YELLOW: string = 'yellow';
 const ORANGE: string = 'orange';
 const PURPLE: string = 'purple';
 
-class MapChar {
+class ColorChar {
     public char: string;
     public color: string;
 
@@ -16,21 +16,42 @@ class MapChar {
     }
 }
 
+class Coord {
+    public row: number;
+    public col: number;
+
+    constructor (row: number = 0, col: number = 0) {
+        this.row = row;
+        this.col = col;
+    }
+}
+class MapObject {
+    public colorChar: ColorChar;
+    public coord: Coord;
+
+    constructor (colorChar: ColorChar, coord: Coord) {
+        this.colorChar = colorChar;
+        this.coord = coord;
+    }
+}
+
 window.onload = function () {
     const WINDOW_ID: string = 'game_window';
-    const COLUMNS: number = 99;
     const ROWS: number = 26;
+    const COLS: number = 99;
     const bgChar: string = '-';
     const heroChar: string = '@';
-    let objects: MapChar[] = [new MapChar(heroChar, GREEN)];
-    let objectCoords: number[] = [0,0];
 
-    /* make one div per line */
-    let idArray: any[] = addDivRows(WINDOW_ID, ROWS, COLUMNS);
-    const map: any[] = makeMap(COLUMNS, ROWS, bgChar);
-    draw(idArray, map, objects, objectCoords);
+    let objects: MapObject[] = [new MapObject(
+        new ColorChar(heroChar, GREEN), 
+        new Coord()
+    )];
 
-    document.addEventListener('keydown', function keyHandler (event: any) {
+    let idArray: any[] = insertDivsSpans(WINDOW_ID, ROWS, COLS);
+    const map: any[] = makeMap(COLS, ROWS, bgChar);
+    draw(idArray, map, objects);
+
+    /*document.addEventListener('keydown', function keyHandler (event: any) {
         if (event.defaultPrevented) {
             return; // Do nothing if the event was already processed
         }
@@ -39,34 +60,34 @@ window.onload = function () {
         case 40: // down arrow
         case 74: // j
         case 83: // s
-            if (objectCoords[0] < ROWS - 1) {
-                objectCoords[0]++;
+            if (objects[1].coord[0] < ROWS - 1) {
+                objects[1].coord[0]++;
             }
-            draw(idArray, map, objects, objectCoords);
+            draw(idArray, map, objects);
             break;
         case 38: // up arrow
         case 75: // k
         case 87: // w
-            if (objectCoords[0] > 0) {
-                objectCoords[0]--;
+            if (objects[1].coord[0] > 0) {
+                objects[1].coord[0]--;
             }
-            draw(idArray, map, objects, objectCoords);
+            draw(idArray, map, objects);
             break;
         case 37: // left arrow
         case 72: // h
         case 65: // a
-            if (objectCoords[1] > 0) {
-                objectCoords[1]--;
+            if (objects[1].coord[1] > 0) {
+                objects[1].coord[1]--;
             }
-            draw(idArray, map, objects, objectCoords);
+            draw(idArray, map, objects);
             break;
         case 39: // right arrow
         case 76: // l
         case 68: // d
-            if (objectCoords[1] < COLUMNS - 1) {
-                objectCoords[1]++;
+            if (objects[1].coord[1] < COLS - 1) {
+                objects[1].coord[1]++;
             }
-            draw(idArray, map, objects, objectCoords);
+            draw(idArray, map, objects);
             break;
         default:
             return; // Quit when this doesn't handle the key event.
@@ -74,7 +95,7 @@ window.onload = function () {
     
         // Cancel the default action to avoid it being handled twice
         event.preventDefault();
-    }, true);
+    }, true);*/
         
     //  ENTER GAME_LOOP
     //      UPDATE
@@ -86,27 +107,25 @@ function makeMap (cols: number, rows: number, bgChar: string): any[] {
     for (let i = 0; i < rows; i++) {
         array[i] = [];
         for (let j = 0; j < cols; j++) {
-            array[i][j] = new MapChar(bgChar);
+            array[i][j] = new ColorChar(bgChar);
         }
     }
     return array;
 }
 
-function applyObjects (map: any[], objects: MapChar[], objectCoords: number[]): any[] {
-    // copy 2d array
+function applyObjects (map: any[], objects: MapObject[]): any[] {
     let newMap: any[] = [];
     for (let i = 0; i < map.length; i++) {
         newMap[i] = map[i].slice();
     }
-    // insert objects
     for (let i = 0; i < objects.length; i++) {
-        newMap[objectCoords[i*2]][objectCoords[i*2+1]] = objects[i];
+        newMap[objects[i].coord.row][objects[i].coord.col] = objects[i].colorChar;
     }
     return newMap;
 }
 
-function draw (idArray: any[], map: any[], objects: MapChar[], objectCoords: number[]): void {
-    let mapToDraw = applyObjects(map, objects, objectCoords);
+function draw (idArray: any[], map: any[], objects: MapObject[]): void {
+    let mapToDraw = applyObjects(map, objects);
     for (let i = 0; i < idArray.length; i++) {
         for (let j = 0; j < idArray[i].length; j++) {
             let span = document.getElementById(idArray[i][j]);
@@ -116,7 +135,7 @@ function draw (idArray: any[], map: any[], objects: MapChar[], objectCoords: num
     }
 }
 
-function addDivRows (windowId: string, rows: number, cols: number): any[] {
+function insertDivsSpans (windowId: string, rows: number, cols: number): any[] {
     let idArray: any[] = [];
     for (let i = 0; i < rows; i++) {
         idArray[i] = [];
